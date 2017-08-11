980e260a851813fa7dd460dcc2d5514b5d813f8c
Paulo Lourenco <paulo@lourenco.net.au>
2017-08-11 17:10:40 +1000

    revised beachie weightings

--- app/js/scoremyride.js
+++ app/js/scoremyride.js
@@ -3,12 +3,12 @@ var theScore = 0.0;
 reset();
 
 //default score profile (shifting beachie)
-var beachies             = {supertube:  8.5,  superturn:  6.0, superhuman: 8.5,
-                            mediumtube: 6.0,  mediumturn: 4.0,
-                            smalltube:  4.0,  smallturn:  2.0};
-var longHollowPoint      = {supertube:  7.0,  superturn:  5.0, superhuman: 6.5,
-                            mediumtube: 4.0,  mediumturn: 3.0,
-                            smalltube:  3.0,  smallturn:  2.0};
+var beachies             = {supertube:  8.5,  superturn:  4.0, superhuman: 6.5,
+                            mediumtube: 4.0,  mediumturn: 2.0,
+                            smalltube:  2.0,  smallturn:  1.0};
+var longHollowPoint      = {supertube:  5.0,  superturn:  3.0, superhuman: 5.5,
+                            mediumtube: 4.0,  mediumturn: 2.0,
+                            smalltube:  3.0,  smallturn:  1.0};
 var longPerformancePoint = {supertube:  8.5,  superturn:  4.0, superhuman: 6.5,
                             mediumtube: 5.0,  mediumturn: 3.0,
                             smalltube:  4.0,  smallturn:  2.0};
